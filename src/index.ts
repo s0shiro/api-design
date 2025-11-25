2eeddf0bef37093d2c82194383ff670d056a5d04
kaizen <mascarinas022@gmail.com>
2025-11-25 10:24:39 +0800

    add initial configuration and setup for environment variables, database, and server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from './server.ts'
+import { env } from '../env.ts'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+app.listen(env.PORT, () => {
   console.log(`API listening on port ${PORT}`)
 })
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.ts'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { isTest } from '../env.ts'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  }),
+)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/health', (req, res) => {
   res.json({ message: 'Hello there!' })
 })
+
+app.use('/api/auth', authRoutes)
 
 export default app
