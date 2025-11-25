86e845365a9b07684f19cbbe28d7defd263c2e37
kaizen <mascarinas022@gmail.com>
2025-11-25 23:02:45 +0800

    add authentication middleware to verify JWT tokens and attach user payload

---
+++ src/middleware/auth.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from 'express'
+import { verifyToken, type JWTPayload } from '../utils/jwt.ts'
+
+export interface AuthenticatedRequest extends Request {
+  user?: JWTPayload
+}
+
+export const authenticateToken = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+      return res.status(401).json({ error: 'Bad Request.' })
+    }
+
+    const payload = await verifyToken(token)
+    req.user = payload
+    next()
+  } catch (error) {}
+}
